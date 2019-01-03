478d6e4197ebe70c5149dea00b2db1b31c37e36c
a7md940 <ahmedatefangelov@gmail.com>
2019-01-03 19:24:45 +0200

    enhance styles and grid.

--- src/app/navbar/navbar.component.ts
+++ src/app/navbar/navbar.component.ts
@@ -6,10 +6,20 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./navbar.component.scss']
 })
 export class NavbarComponent implements OnInit {
-
   constructor() { }
 
   ngOnInit() {
+    // close navbar after click nav item link.
+    // array of nav items link.
+    const navItems = Array.from(document.getElementsByClassName('nav-item'));
+    // for each item add click event listner to it.
+    navItems.forEach((item, i) => {
+      item.addEventListener('click', itemClick, false);
+    });
+    // triggre checkbox function.
+    function itemClick(e) {
+      document.getElementById('c__navbar-toggler').click();
+    }
   }
 
 }
